7261ba6858a36190788e39212ecb6ed694203b37
Katz <grigorii_seredin@mail.ru>
2020-06-30 03:01:11 +0300

    Added postman-module

--- course/ml-api/addfuncs.py
+++ course/ml-api/addfuncs.py
@@ -10,47 +10,47 @@ def mapfordictgender(gender):
     return res
 
 
-def mapfordictmaristat(MariStat):
+def mapfordictmaristat(maristat):
     """преобразуемый признак - семейное положение"""
-    dict_MariStat = {'Other': 0, 'Alone': 1}
-    res = dict_MariStat.get(MariStat)
+    dictmaristat = {'Other': 0, 'Alone': 1}
+    res = dictmaristat.get(maristat)
     return res
 
 
-def fvehusageprofessional(VehUsage):
+def fvehusageprofessional(vehusage):
     """преобразуемый признак - профессиональное использование"""
-    if VehUsage == 'Professional':
-        VehUsage_Professional = 1
+    if vehusage == 'Professional':
+        vehusageprofessional = 1
     else:
-        VehUsage_Professional = 0
-    return VehUsage_Professional
+        vehusageprofessional = 0
+    return vehusageprofessional
 
 
-def fvehusageprivatetriptooffice(VehUsage):
+def fvehusageprivatetriptooffice(vehusage):
     """преобразуемый признак - использование транспорта (личное+дорога до офиса)"""
-    if VehUsage == 'Private+trip to office':
-        VehUsage_Private_trip_to_office = 1
+    if vehusage == 'Private+trip to office':
+        vehusageprivatetriptooffice = 1
     else:
-        VehUsage_Private_trip_to_office = 0
-    return VehUsage_Private_trip_to_office
+        vehusageprivatetriptooffice = 0
+    return vehusageprivatetriptooffice
 
 
-def fvehusageprivate(VehUsage):
+def fvehusageprivate(vehusage):
     """преобразуемый признак - использование транспорта (личное)"""
-    if VehUsage == 'Private':
-        VehUsage_Private = 1
+    if vehusage == 'Private':
+        vehusageprivate = 1
     else:
-        VehUsage_Private = 0
-    return VehUsage_Private
+        vehusageprivate = 0
+    return vehusageprivate
 
 
-def fvehusageprofessionalrun(VehUsage):
+def fvehusageprofessionalrun(vehusage):
     """преобразуемый признак - профессиональное использование"""
-    if VehUsage == 'Professional run':
-        VehUsage_Professional_run = 1
+    if vehusage == 'Professional run':
+        vehusageprofessionalrun = 1
     else:
-        VehUsage_Professional_run = 0
-    return VehUsage_Professional_run
+        vehusageprofessionalrun = 0
+    return vehusageprofessionalrun
 
 
 def returnnewh2oframe():
@@ -132,80 +132,80 @@ def returnnewh2oframe():
 
 def processinput(json_input):
     """читаем json"""
-    LicAge = json_input["LicAge"]
-    Gender = mapfordictgender(json_input["Gender"])
-    MariStat = mapfordictmaristat(json_input["MariStat"])
-    DrivAge = json_input["DrivAge"]
-    HasKmLimit = json_input["HasKmLimit"]
-    BonusMalus = json_input["BonusMalus"]
-    OutUseNb = json_input["OutUseNb"]
-    RiskArea = json_input["RiskArea"]
-    VehUsg_Private = fvehusageprivate(json_input["VehUsage"])
-    VehUsg_Private_trip_to_office = fvehusageprivatetriptooffice(
+    licage = json_input["LicAge"]
+    gender = mapfordictgender(json_input["Gender"])
+    maristat = mapfordictmaristat(json_input["MariStat"])
+    drivage = json_input["DrivAge"]
+    haskmlimit = json_input["HasKmLimit"]
+    bonusmalus = json_input["BonusMalus"]
+    outusenb = json_input["OutUseNb"]
+    riskarea = json_input["RiskArea"]
+    vehusgprivate = fvehusageprivate(json_input["VehUsage"])
+    vehusgprivatetriptooffice = fvehusageprivatetriptooffice(
         json_input["VehUsage"])
-    VehUsg_Professional = fvehusageprofessional(json_input["VehUsage"])
-    VehUsg_Professional_run = fvehusageprofessionalrun(
+    vehusgprofessional = fvehusageprofessional(json_input["VehUsage"])
+    vehusgprofessionalrun = fvehusageprofessionalrun(
         json_input["VehUsage"])
 
-    CSP1 = 0
-    CSP2 = 0
-    CSP3 = 0
-    CSP6 = 0
-    CSP7 = 0
-    CSP20 = 0
-    CSP21 = 0
-    CSP22 = 0
-    CSP26 = 0
-    CSP37 = 0
-    CSP40 = 0
-    CSP42 = 0
-    CSP46 = 0
-    CSP47 = 0
-    CSP48 = 0
-    CSP49 = 0
-    CSP50 = 0
-    CSP55 = 0
-    CSP56 = 0
-    CSP57 = 0
-    CSP60 = 0
-    CSP65 = 0
-    CSP66 = 0
+    csp1 = 0
+    csp2 = 0
+    csp3 = 0
+    csp6 = 0
+    csp7 = 0
+    csp20 = 0
+    csp21 = 0
+    csp22 = 0
+    csp26 = 0
+    csp37 = 0
+    csp40 = 0
+    csp42 = 0
+    csp46 = 0
+    csp47 = 0
+    csp48 = 0
+    csp49 = 0
+    csp50 = 0
+    csp55 = 0
+    csp56 = 0
+    csp57 = 0
+    csp60 = 0
+    csp65 = 0
+    csp66 = 0
 
-    hf = returnnewh2oframe()
+    frame = returnnewh2oframe()
 
-    hf[0, 'LicAge'] = LicAge
-    hf[0, 'Gender'] = Gender
-    hf[0, 'MariStat'] = MariStat
-    hf[0, 'DrivAge'] = DrivAge
-    hf[0, 'HasKmLimit'] = HasKmLimit
-    hf[0, 'BonusMalus'] = BonusMalus
-    hf[0, 'OutUseNb'] = OutUseNb
-    hf[0, 'RiskArea'] = RiskArea
-    hf[0, 'VehUsg_Private'] = VehUsg_Private
-    hf[0, 'VehUsg_Private+trip to office'] = VehUsg_Private_trip_to_office
-    hf[0, 'VehUsg_Professional'] = VehUsg_Professional
-    hf[0, 'VehUsg_Professional run'] = VehUsg_Professional_run
-    hf[0, 'CSP1'] = CSP1
-    hf[0, 'CSP2'] = CSP2
-    hf[0, 'CSP3'] = CSP3
-    hf[0, 'CSP6'] = CSP6
-    hf[0, 'CSP7'] = CSP7
-    hf[0, 'CSP20'] = CSP20
-    hf[0, 'CSP21'] = CSP21
-    hf[0, 'CSP22'] = CSP22
-    hf[0, 'CSP26'] = CSP26
-    hf[0, 'CSP37'] = CSP37
-    hf[0, 'CSP40'] = CSP40
-    hf[0, 'CSP42'] = CSP42
-    hf[0, 'CSP46'] = CSP46
-    hf[0, 'CSP47'] = CSP47
-    hf[0, 'CSP48'] = CSP48
-    hf[0, 'CSP49'] = CSP49
-    hf[0, 'CSP50'] = CSP50
-    hf[0, 'CSP55'] = CSP55
-    hf[0, 'CSP56'] = CSP56
-    hf[0, 'CSP57'] = CSP57
-    hf[0, 'CSP60'] = CSP60
-    hf[0, 'CSP65'] = CSP65
-    hf[0, 'CSP66'] = CSP66
-    return hf
+    frame[0, 'LicAge'] = licage
+    frame[0, 'Gender'] = gender
+    frame[0, 'MariStat'] = maristat
+    frame[0, 'DrivAge'] = drivage
+    frame[0, 'HasKmLimit'] = haskmlimit
+    frame[0, 'BonusMalus'] = bonusmalus
+    frame[0, 'OutUseNb'] = outusenb
+    frame[0, 'RiskArea'] = riskarea
+    frame[0, 'VehUsg_Private'] = vehusgprivate
+    frame[0, 'VehUsg_Private+trip to office'] = vehusgprivatetriptooffice
+    frame[0, 'VehUsg_Professional'] = vehusgprofessional
+    frame[0, 'VehUsg_Professional run'] = vehusgprofessionalrun
+    frame[0, 'CSP1'] = csp1
+    frame[0, 'CSP2'] = csp2
+    frame[0, 'CSP3'] = csp3
+    frame[0, 'CSP6'] = csp6
+    frame[0, 'CSP7'] = csp7
+    frame[0, 'CSP20'] = csp20
+    frame[0, 'CSP21'] = csp21
+    frame[0, 'CSP22'] = csp22
+    frame[0, 'CSP26'] = csp26
+    frame[0, 'CSP37'] = csp37
+    frame[0, 'CSP40'] = csp40
+    frame[0, 'CSP42'] = csp42
+    frame[0, 'CSP46'] = csp46
+    frame[0, 'CSP47'] = csp47
+    frame[0, 'CSP48'] = csp48
+    frame[0, 'CSP49'] = csp49
+    frame[0, 'CSP50'] = csp50
+    frame[0, 'CSP55'] = csp55
+    frame[0, 'CSP56'] = csp56
+    frame[0, 'CSP57'] = csp57
+    frame[0, 'CSP60'] = csp60
+    frame[0, 'CSP65'] = csp65
+    frame[0, 'CSP66'] = csp66
+    return frame
